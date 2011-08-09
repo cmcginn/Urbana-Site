09b6ef32cdcc22223253de72db92bc54d5127360
Chris <chris.s.mcginn@live.com>
2011-08-09 05:52:38 -0400

    dfdssd

--- Urbana/Urbana/Urbana/Controllers/HomeController.cs
+++ Urbana/Urbana/Urbana/Controllers/HomeController.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
 using System.Web.Mvc;
-
+using Urbana.Models;
 namespace Urbana.Controllers
 {
     public class HomeController : Controller
@@ -18,6 +19,39 @@ namespace Urbana.Controllers
         public ActionResult About()
         {
             return View();
+        }
+        public ActionResult Contact()
+        {
+            return View(new ContactModel());
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var m = new System.Net.Mail.MailMessage();
+                m.From = new System.Net.Mail.MailAddress("postmaster@information-datasystems.com");
+                m.To.Add(new System.Net.Mail.MailAddress("chris.s.mcginn@live.com"));
+
+                m.Subject =  String.Format("Urbana inquiry from {0}", model.Email);
+                var builder = new System.Text.StringBuilder();
+                builder.AppendLine(String.Format("First Name:{0}", model.FirstName));
+                builder.AppendLine(String.Format("Last Name:{0}", model.LastName));
+                builder.AppendLine(String.Format("Email:{0}", model.Email));
+                builder.AppendLine(String.Format("Company:{0}", model.Company));
+                builder.AppendLine(String.Format("Phone:{0}-{1}-{2}", model.PhoneAreaCode,model.PhonePrefix,model.PhoneSuffix));
+                builder.AppendLine(String.Format("Comment:{0}", model.Comment));
+
+                var mailClient = new System.Net.Mail.SmtpClient();
+                mailClient.UseDefaultCredentials = true;
+                mailClient.Send(m);
+            
+               
+                return RedirectToAction("Index");
+                
+            }
+            else
+                return View(model);
         }
     }
 }
---
+++ Urbana/Urbana/Urbana/Models/HomeModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+namespace Urbana.Models
+{
+    public class ContactModel
+    {
+        [Display(Name="First Name")]
+        [Required]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Email")]
+        [Required]
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string PhonePrefix { get; set; }
+        public string PhoneAreaCode { get; set; }
+        public string PhoneSuffix { get; set; }
+        [Required]
+        public string Comment { get; set; }
+    }
+}
